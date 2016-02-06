9f3f4c29280ea49fef886d1ff699555ac59e14d1
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-06 11:04:24 -0800

    comments

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -1,5 +1,12 @@
-price = input("What is the price before tax? ")
-tax_percent = input("What are the taxes? (in %) ")
-tip_percent = input("What do you to tip? (in %) ")
+'''
+Daniel Alzugaray
+'''
 
+price = input("What is the price before tax? ") # get the price
+tax_percent = input("What are the taxes? (in %) ") # get the tax percentage as an integer
+tip_percent = input("What do you to tip? (in %) ") # get the tip percentage as an integer
+
+# take the price, and apply the tax first (price with applied tax)
+# after the tax is applied, the tip is calculated from the price including tax, not just original price
+# the division by 100 twice results in a simplified formula: 10,000 = 100 * 100
 print "The price you need to pay is:",price*(100+tax_percent)*(100+tip_percent)/10000
