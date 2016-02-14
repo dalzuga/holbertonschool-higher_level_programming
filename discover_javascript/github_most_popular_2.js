5a444ff9696ce40bcf3a5473a38682e7ed2d6c23
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-13 16:55:31 -0800

    Let's finally have a party!

--- discover_javascript/github_most_popular_2.js
+++ discover_javascript/github_most_popular_2.js
@@ -1,4 +1,19 @@
-https.request(options, function(stream) {
+var https = require('https');
+
+var options = {
+    hostname: 'api.github.com',
+    path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
+    headers: {
+	'User-Agent': 'Holberton_School',
+	'Authorization': 'token 6e73c90fdfe07bca58b4c3e20eaa15e367169857'
+    }
+};
+
+var cb = function(s) {
+    console.log(s);
+}
+
+var req = https.request(options, function(stream) {
     const chunks = [];
     stream.on('data', (chunk) => {
 	chunks.push(chunk);
