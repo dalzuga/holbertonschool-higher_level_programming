cf1ae5301b8d12d254968e6b5721cb05b07395fe
Daniel Alzugaray <49@p7-3.sfo1.hbtn.io>
2016-02-13 16:46:13 -0800

    let's have a party!

---
+++ discover_javascript/github_most_popular_2.js
@@ -0,0 +1,10 @@
+https.request(options, function(stream) {
+    const chunks = [];
+    stream.on('data', (chunk) => {
+	chunks.push(chunk);
+    });
+    stream.on('end', () => {
+	cb(chunks.join(''));
+    });
+});
+req.end();
