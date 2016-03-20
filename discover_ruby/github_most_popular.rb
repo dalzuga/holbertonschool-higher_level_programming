b27e80e564f2ec88e7d32bade5d93d6b7b5af3ae
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-19 21:18:29 -0700

    trying task 1

--- discover_ruby/github_most_popular.rb
+++ discover_ruby/github_most_popular.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/ruby
+
+require "net/http"
+require "uri"
+
 extheaders = {
   'User-Agent' => 'Holberton_School',
   'Authorization' => 'token 6e73c90fdfe07bca58b4c3e20eaa15e367169857'
 }
+
+uri = URI.parse("https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc")
+
+# puts uri.scheme
+# puts uri.host
+# puts uri.path
+# puts uri.query
+
+response = Net::HTTP.get(uri)
+
+puts response
