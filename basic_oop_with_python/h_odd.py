13415175c40f8e16df1d721db18e4d0e14d9bc4a
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-17 20:24:05 -0700

    task 1: handled case where odd parameter is not an int

--- basic_oop_with_python/h_odd.py
+++ basic_oop_with_python/h_odd.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 ''' Returns 1 if odd, 0 if even '''
 def odd(x):
-    if (x % 2 == 0):
+    if type(x) is not int:
         return 0
-    else:
+
+    if (x % 2 == 1):
         return 1
+    else:
+        return 0
