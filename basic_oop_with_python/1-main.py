68639c017acf400295d25eb2b2d3b27b00029181
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-17 17:03:07 -0700

    task 1 done

---
+++ basic_oop_with_python/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from h_odd import odd
+
+print "%d" % odd(1)
+print "%d" % odd(5) 
+print "%d" % odd(4)
+print "%d" % odd(95)
+print "%d" % odd(104)
--- basic_oop_with_python/h_odd.py
+++ basic_oop_with_python/h_odd.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python
+def odd(x):
+    if (x % 2 == 0):
+        return 0
+    else:
+        return 1
