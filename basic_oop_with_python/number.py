fef98bdc167a3c072c9b67aabef907be9c783a77
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-18 06:39:51 -0700

    task 7 done

--- basic_oop_with_python/number.py
+++ basic_oop_with_python/number.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python
+import math
+
+"""Class"""
+class Number(object):
+    """Constructs a number."""
+    def __init__(self, value):
+        self.__value = value
+
+    def set_value(self, sum):
+        self.__value = sum
+
+    def get_value(self):
+        return self.__value
+
+    """Redefine operators."""
+    def __add__(self,other):
+        return Number(self.__value + other.__value)
+
+    def __sub__(self,other):
+        return Number(self.__value - other.__value)
+
+    def __mul__(self,other):
+        return Number(self.__value * other.__value)
+
+    def __div__(self,other):
+        return Number(self.__value / other.__value)
+
+    def __str__(self):
+        return str(self.__value)
