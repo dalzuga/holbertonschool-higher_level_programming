d9875b0923485d2bc06a7315ff30d005c573b7e3
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-18 03:41:10 -0700

    added comments and init overlap function

--- basic_oop_with_python/circle.py
+++ basic_oop_with_python/circle.py
@@ -28,23 +28,30 @@ class Circle():
         '''sum of the radii'''
         sum_radii = self.__radius + c_bis.__radius
 
+        ''' center distance with respect to x-axis and y-axis '''
         delta_x = c_bis.get_center()[0] - self.get_center()[0]
         delta_y = c_bis.get_center()[1] - self.get_center()[1]
 
         ''' if r^2 < a^2 + b^2 '''
-        ''' they do not intersect '''
+        ''' they overlap '''
         if (sum_radii ** 2 < delta_x ** 2 + delta_y ** 2):
             return 0
         
         ''' otherwise '''
-        ''' they intersect '''
+        ''' they do not overlap '''
         return 1
+
+    def overlap(self, c_bis):
+        ''' center distance with respect to x-axis and y-axis '''
+        delta_x = c_bis.get_center()[0] - self.get_center()[0]
+        delta_y = c_bis.get_center()[1] - self.get_center()[1]
 
     def intersection_percentage(self, c_bis):
 
         if (self.intersection(c_bis) == 0):
             return 0;
 
+        ''' center distance with respect to x-axis and y-axis '''
         delta_x = c_bis.get_center()[0] - self.get_center()[0]
         delta_y = c_bis.get_center()[1] - self.get_center()[1]
         r = self.__radius
