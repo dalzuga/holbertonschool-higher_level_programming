a9fd39e5979421acad2fe30d69e784acdc379913
Daniel Alzugaray <dalzuga@gmail.com>
2016-06-08 01:38:11 +0000

    task 2 done

---
+++ basic_sql_playground/2-main.sql
@@ -0,0 +1,3 @@
+insert into person (first_name, last_name, age) values ('Jon', 'Snow', 26);
+insert into person (first_name, last_name, age) values ('Arya', 'Stark', 12);
+select * from person order by last_name;
