a2f472bf3fce616acbdb94d05575bdcce7dae2bb
Daniel Alzugaray <dalzuga@gmail.com>
2016-06-08 05:51:02 +0000

    hardcoding id

--- basic_sql_playground/3-main.sql
+++ basic_sql_playground/3-main.sql
@@ -1,9 +1,5 @@
 UPDATE Person SET age=27 WHERE last_name='Snow' AND first_name='Jon';
 UPDATE Person SET age=18 WHERE last_name='White' AND first_name='Walter Junior';
 
-/*DELETE FROM Person JOIN EyesColor ON Person.id = EyesColor.person_id WHERE last_name='White' AND first_name='Walter';*/
-
-/*SELECT id INTO temp FROM Person JOIN EyesColor ON Person.id = EyesColor.person_id;*/
-
-DELETE FROM Person WHERE last_name='White' AND first_name='Walter';
-DELETE FROM EyesColor WHERE last_name='White' AND first_name='Walter';
+DELETE FROM Person WHERE id=1;
+DELETE FROM EyesColor WHERE person_id=1;
