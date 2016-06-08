f4c9c2c7d1e788f8e3127eb68cc2cc533ebd9c69
Daniel Alzugaray <dalzuga@gmail.com>
2016-06-08 16:58:08 -0500

    task 0 done

--- databases_are_so_easy_to_manage_in_command_line/main.py
+++ databases_are_so_easy_to_manage_in_command_line/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python
+import sys
+if len(sys.argv) < 2:
+    print "Please enter an action"
+elif (sys.argv[1] == "create"):
+    print "create"
+elif (sys.argv[1] == "print"):
+    print "print"
+elif (sys.argv[1] == "insert"):
+    print "insert"
+elif (sys.argv[1] == "delete"):
+    print "delete"
+else:
+    print "Undefined action " + sys.argv[1]
