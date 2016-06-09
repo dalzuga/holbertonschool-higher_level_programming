65ffb08be9e3201f58034a19ff23ef5ad0f86a79
Daniel Alzugaray <dalzuga@gmail.com>
2016-06-09 03:53:46 -0500

    saving progress

--- databases_are_so_easy_to_manage_in_command_line/main.py
+++ databases_are_so_easy_to_manage_in_command_line/main.py
@@ -2,16 +2,34 @@
 import sys
 import models
 
-''' check for usage '''
-
+''' check for usage and process input '''
 if len(sys.argv) < 2:
     print "Please enter an action"
 elif (sys.argv[1] == "create"):
     models.BaseModel.create_table()
     print "create"
 elif (sys.argv[1] == "print"):
-    print "print"
+    if len(sys.argv) < 3:
+        pass
+    elif (sys.argv[2] == "school"):
+        print models.my_models_db.School()
+    elif (sys.argv[2] == "batch"):
+        print models.my_models_db.Batch()
+    elif (sys.argv[2] == "user"):
+        print models.my_models_db.User()
+    elif (sys.argv[2] == "student"):
+        print models.my_models_db.Student()
 elif (sys.argv[1] == "insert"):
+    if len(sys.argv) < 3:
+        pass
+    elif (sys.argv[2] == "school"):
+        models.School.create_table();
+    elif (sys.argv[2] == "batch"):
+        models.School.create_table();
+    elif (sys.argv[2] == "user"):
+        models.School.create_table();
+    elif (sys.argv[2] == "student"):
+        models.School.create_table();
     print "insert"
 elif (sys.argv[1] == "delete"):
     print "delete"
