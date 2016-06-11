1b795545c8978c3b81413d5a2ce43ef7759c4db2
Daniel Alzugaray <dalzuga@gmail.com>
2016-06-11 02:39:14 -0500

    removed trailing semi-colons for the shell test program

--- databases_are_so_easy_to_manage_in_command_line/test.sh
+++ databases_are_so_easy_to_manage_in_command_line/test.sh
@@ -1,16 +1,16 @@
-chmod +x ./main.py;
-rm my_models.db;
-./main.py create;
-./main.py insert school Avengers;
-./main.py insert school GoT;
-./main.py insert batch 1 0116sf;
-./main.py print school;
-./main.py insert batch 2 "winter 2017";
-./main.py insert batch 1 "may batch";
-./main.py insert student 1 25 Alzugaray Daniel;
-./main.py insert student 1 35 Stark Tony;
-./main.py insert student 1 60 Rogers Steve;
-./main.py insert student 2 30 Targaryen Daenerys;
-./main.py insert student 2 15 Stark Sansa;
-./main.py insert student 2 30 "of Tarth" Brienne;
+chmod +x ./main.py
+rm my_models.db
+./main.py create
+./main.py insert school Avengers
+./main.py insert school GoT
+./main.py insert batch 1 0116sf
+./main.py print school
+./main.py insert batch 2 "winter 2017"
+./main.py insert batch 1 "may batch"
+./main.py insert student 1 25 Alzugaray Daniel
+./main.py insert student 1 35 Stark Tony
+./main.py insert student 1 60 Rogers Steve
+./main.py insert student 2 30 Targaryen Daenerys
+./main.py insert student 2 15 Stark Sansa
+./main.py insert student 2 30 "of Tarth" Brienne
 ./main.py print_student_by_school 2
